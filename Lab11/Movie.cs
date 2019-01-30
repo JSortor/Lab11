d51c413f22c8a1a883b31b02dfa6b88de7e77df9
Justin <justinsortor@gmail.com>
2019-01-29 21:51:08 -0500

    First Commit

---
+++ Lab11/Movie.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab11
+{
+    class Movie
+    {
+        private string title;
+        private string category;
+
+        public string Title
+        {
+            get
+            {
+                return title;
+            }
+            set
+            {
+                title = value;
+            }
+        }
+        public string Category
+        {
+            get
+            {
+                return category;
+            }
+            set
+            {
+                category = value;
+            }
+        }
+
+        public Movie (string title, string category)
+        {
+            Title = title;
+            Category = category;
+        }
+
+        public static string[] categories = new string[] { "comedy", "drama", "action", "horror", "family", "romance" };
+    }
+
+    class Validator
+    {
+        public static int CheckNum(string inputString, string errorMessage)
+        {
+            int input;
+            while (!(int.TryParse(inputString, out input)) || int.Parse(inputString) <= 0 || int.Parse(inputString) > 6)
+            {
+                Console.WriteLine($"Im sorry, that's not a valid input. {errorMessage}");
+                inputString = Console.ReadLine();
+            }
+            return input - 1;
+        }
+    }
+}
---
+++ Lab11/Program.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab11
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool runP = true;
+            Console.WriteLine("Hello, and welcome to the amazing movie list application!");
+
+
+            ArrayList movieList = new ArrayList();
+
+            Movie talladegaNights = new Movie("Talladega Nights", "comedy");
+            Movie sevenPounds = new Movie("Seven Pounds", "drama");
+            Movie halloween = new Movie("Halloween", "horror");
+            Movie theIncredibles = new Movie("The Incredibles", "family");
+            Movie saw = new Movie("Saw", "horror");
+            Movie theNotebook = new Movie("The Notebook", "romance");
+            Movie outtaCompton = new Movie("Straight Outta Compton", "drama");
+            Movie stepBrothers = new Movie("Step Brothers", "comedy");
+            Movie gremlins = new Movie("Gremlins", "horror");
+            Movie deadPool = new Movie("Deadpool", "action");
+
+            movieList.Add(talladegaNights);
+            movieList.Add(sevenPounds);
+            movieList.Add(halloween);
+            movieList.Add(theIncredibles);
+            movieList.Add(saw);
+            movieList.Add(theNotebook);
+            movieList.Add(outtaCompton);
+            movieList.Add(stepBrothers);
+            movieList.Add(gremlins);
+            movieList.Add(deadPool);
+
+
+            while (runP)
+            {
+                PrintCategories();
+
+                Console.WriteLine("Enter the number of the genre you are looking for: ");
+                int inputNum = Validator.CheckNum(Console.ReadLine(), "\nPlease enter a number between 1-6: ");
+
+                PrintList(inputNum, movieList);
+
+                runP = KeepRunning("Would you like to continue? (y/n) ");
+            }
+        }
+
+        static void PrintCategories()
+        {
+            for (int i = 0; i < Movie.categories.Length; i++)
+            {
+                Console.WriteLine($"{i + 1} -- {Movie.categories[i]}");
+            }
+        }
+
+        static void PrintList(int numCategory, ArrayList movieList)
+        {
+            Console.WriteLine($"\nYou chose {numCategory + 1} -- {Movie.categories[numCategory]}.\n");
+
+            ArrayList newList = new ArrayList();
+
+            foreach(Movie m in movieList)
+            {
+                if (m.Category == Movie.categories[numCategory])
+                { 
+                    newList.Add(m.Title);
+                }
+            }
+            newList.Sort();
+            Console.WriteLine($"There are {newList.Count} films in this library considered {Movie.categories[numCategory]}:\n");
+
+            foreach (var n in newList)
+            {
+                Console.WriteLine(n);
+            }
+        }
+
+        static bool KeepRunning(string message)
+        {
+            bool correctInput = true;
+            bool continuer = true;
+            do
+            {
+                Console.WriteLine("\n" + message);
+
+                string confirm = Console.ReadLine().ToLower();
+                if (confirm == "n" || confirm == "no")
+                {
+                    Console.WriteLine("\nGoodbye, come back soon!");
+                    continuer = false;
+                    correctInput = true;
+                    Console.ReadKey();
+                }
+                else if (confirm == "y" || confirm == "yes")
+                {
+                    Console.WriteLine("\nSounds Good!\n");
+                    continuer = true;
+                    correctInput = true;
+                }
+                else
+                {
+                    Console.WriteLine("Sorry, I didn't understand.");
+                }
+            } while (!correctInput);
+            return continuer;
+            
+        }
+    }
+}
